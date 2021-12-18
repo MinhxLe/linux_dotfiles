9c6a237bc8fb5e542fd58af3161cc50dda85aa39
Minh Le <minh@parafin.com>
2021-12-18 11:22:32 -0800

    some  fzf stuff

--- .fzf.zsh
+++ .fzf.zsh
@@ -10,4 +10,9 @@ fi
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+if [ -f /usr/share/doc/fzf/examples/key-bindings.zsh ]; then
+    source "/usr/share/doc/fzf/examples/key-bindings.zsh"
+fi
+if [ -f /usr/share/doc/fzf/examples/completion.zsh ]; then
+    source "/usr/share/doc/fzf/examples/completion.zsh"
+fi
